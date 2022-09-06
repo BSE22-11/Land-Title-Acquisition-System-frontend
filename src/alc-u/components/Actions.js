027040ced3357fa313f69fc768eac89bbe0c8338
KawudhaRachealOlive <51445633+KawudhaRachealOlive@users.noreply.github.com>
2022-09-06 09:01:24 -0700

    Modules

---
+++ src/alc-u/components/Actions.js
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import { Link as RouterLink, Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+
+export default function BasicMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        id="basic-button"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+        Actions
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <Link to="/alc/view"><MenuItem onClick={handleClose}> View Application</MenuItem></Link>
+        <Link to="/alc/reject"><MenuItem onClick={handleClose}>Reject</MenuItem></Link>
+        <Link to="/alc/upload"><MenuItem onClick={handleClose}>Upload documents</MenuItem></Link>
+      </Menu>
+    </div>
+  );
+}
+
